8e33c717bad5a47b02d1979b538a759d85a17218
wangzhibo <wangzhibo@sensetime.com>
2020-07-23 19:18:59 +0800

    sync

--- logger.py
+++ logger.py
@@ -7,7 +7,6 @@ import time
 import sys
 import logging
 
-import torch.distributed as dist
 
 
 def setup_logger(logpth):
@@ -15,8 +14,6 @@ def setup_logger(logpth):
     logfile = osp.join(logpth, logfile)
     FORMAT = '%(levelname)s %(filename)s(%(lineno)d): %(message)s'
     log_level = logging.INFO
-    if dist.is_initialized() and not dist.get_rank()==0:
-        log_level = logging.ERROR
     logging.basicConfig(level=log_level, format=FORMAT, filename=logfile)
     logging.root.addHandler(logging.StreamHandler())
 
