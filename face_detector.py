9ed3abff2ebd71258fb1a39156c401da04fdb6df
wangzhibo <wangzhibo@sensetime.com>
2020-07-26 16:57:42 +0800

    sync

--- face_detector.py
+++ face_detector.py
@@ -11,11 +11,13 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 faces_list = []
 for filename in imglist:
     img = cv2.imread(filename)
-    
+    print(img.shape)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # 进行人脸检测
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     faces_list.append(faces)
+    print(faces)
+    break
 
 faces_list = np.asarray(faces_list)
 np.save("./ffhq.npy", faces_list)
